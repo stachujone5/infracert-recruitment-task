7f22d448c859a4673e93563e887c8be7263d4a73
Stanisław Jakubowski <sjakubowski3554@gmail.com>
2022-09-10 08:49:59 +0200

    Chore: Install packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='form-control w-full max-w-md'>
+      <form className=''>
+        <label className='label'>
+          <span className='label-text'>What is your name?</span>
+        </label>
+        <input type='text' placeholder='Type here' className='input input-bordered w-full max-w-xs' />
+      </form>
     </div>
-  );
+  )
 }
-
-export default App;
